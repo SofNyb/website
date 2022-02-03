d124895e767bbf6a63a3be6e40dc2f74d56dc193
snjwi <sofi13a5@edu.zealand.dk>
2022-02-03 13:02:59 +0100

    git init

---
+++ kontakt.php
@@ -0,0 +1,30 @@
+<!-- Instruktion til webbrowser om at vi kører HTML5 -->
+<!DOCTYPE html>
+
+<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
+<html lang="da">
+
+<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
+<head>
+    <meta charset="utf-8">
+    <title>Sigende titel</title>
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php include "includes/navigation.php"; ?>
+
+<div class="main">
+    <div>
+        kontakt
+    </div>
+</div>
+
+<?php include "includes/footer.php"; ?>
+
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    <title>Sigende titel</title>
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<div class="card__header"> <!--Header-->
+
+    <div class="card__logo">Logo</div> <!--Logo-->
+
+    <div class="card__bar"> <!--search-bar-->
+        <input class="bar__input" type="text"> <!--search-bar input-->
+        <span class="bar__btn">Søg</span> <!--search-bar button-->
+    </div>
+
+    <div class="card__nav"> <!--navigation-->
+
+        <div class="nav__btn"> <!--navigation button-->
+            <a href="#" class="nav__link">Forside</a> <!--navigation link-->
+        </div>
+
+        <div class="nav__btn"> <!--navigation button-->
+            <a href="#" class="nav__link">Om os</a> <!--navigation link-->
+        </div>
+
+        <div class="nav__btn"> <!--navigation button-->
+            <a href="#" class="nav__link">Kontakt</a> <!--navigation link-->
+        </div>
+    </div>
+
+</div>
+
+
+<div class="card__wrapper"> <!--card wrapper-->
+
+    <div class="card"> <!--card-->
+
+        <div class="card__header"> <!--card header-->
+            <h2 class="card__title">Title text here</h2> <!--card title-->
+        </div>
+
+        <div class="card__body"> <!--card body-->
+
+            <img class="card_img" src="some-img.png" alt="description"> <!--card image-->
+
+            <p class="card__text">Lorem ipsum dolor sit amet, consectetur</p> <!--card text-->
+
+            <a class="card__text" href="#">Pellentesque amet</a> <!--card link-->
+
+        </div>
+    </div>
+
+    <div class="card"> <!--card-->
+
+        <div class="card__header"> <!--card header-->
+            <h2 class="card__title">Title text here</h2> <!--card title-->
+        </div>
+
+        <div class="card__body"> <!--card body-->
+
+            <img class="card__img" src="some-img.png" alt="description"> <!--card image-->
+
+            <p class="card__text">Lorem ipsum dolor sit amet, consectetur</p> <!--card text-->
+
+            <a class="card__text" href="#">Pellentesque amet</a> <!--card link-->
+
+        </div>
+    </div>
+
+    <div class="card"> <!--card-->
+
+        <div class="card__header"> <!--card header-->
+            <h2 class="card__title">Title text here</h2> <!--card title-->
+        </div>
+
+        <div class="card__body"> <!--card body-->
+
+            <img class="card__img" src="some-img.png" alt="description"> <!--card image-->
+
+            <p class="card__text">Lorem ipsum dolor sit amet, consectetur</p> <!--card text-->
+
+            <a class="card__text" href="#">Pellentesque amet</a> <!--card link-->
+
+        </div>
+    </div>
+
+</div>
+
+
+<div class="footer"> <!--footer-->
+
+    <div class="footer__list"> <!--footer links list-->
+        <a class="footer__link" href="#">Link</a> <!--footer link-->
+        <a class="footer__link" href="#">Link</a> <!--footer link-->
+        <a class="footer__link" href="#">Link</a> <!--footer link-->
+    </div>
+
+    <div class="footer__address"> <!--footer address-->
+        Company
+        12 Somestreet, 3456 City
+        <a class="footer__link" href="#">Find us</a> <!--footer link-->
+    </div>
+
+    <div class="news"> <!--newsletter-->
+        <input class="news__name" type="text"> <!--newsletter name-->
+        <input class="news_mail" type="email"> <!--newsletter email-->
+    </div>
+
+</div>
+
+</body>
+</html>
